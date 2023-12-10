d9b95d04cfb1ef559d9fa2162b5a19518d72ec04
mmozos <martinmozos@gmail.com>
2023-12-10 19:24:46 +0100

    Keep the only one that's worth it

--- acer-laptop-landing-page/src/components/Container/Container.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./container.css";
-
-export default function Container({ type, children }) {
-  return <div className={`container ${type && `${type}`}`}>{children}</div>;
-}
